afb868f8fdbbce0a7dbd5c56a145d34b44378c14
GASHIWINGO <117731094+GASHIWINGO@users.noreply.github.com>
2023-05-29 17:33:57 +0300

    Update weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -49,7 +49,7 @@ class app(Tk):
                     city_name = response_wa['location']['name']
                     cur_title.config(text=f"Погода {city_name}")
 
-                    description = response_wa['current']['condition']['text']
+                    description = response_wa["forecast"]["forecastday"][0]["day"]["condition"]["text"]
                     cur_descr.config(text=description.title())
                 except:
                     messagebox.showerror('Ошибка!', 'Проверьте название города!')
